c9d372273d641cabc4a80d2090994ebf9a149800
Lekaan <kaans@DESKTOP-U4CBDT5>
2017-01-17 09:38:08 +0100

    Added facade

---
+++ PrettyHair.Core/Interfaces/IFacade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrettyHair.Core.Interfaces
+{
+    interface IFacade
+    {
+        void CreateCustomer(ICustomer customer);
+    }
+}
---
+++ PrettyHair.Core/Repositories/RepositoryFacade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrettyHair.Core.Interfaces;
+
+namespace PrettyHair.Core.Repositories
+{
+    class RepositoryFacade : IFacade
+    {
+        public void CreateCustomer(ICustomer customer)
+        {
+            CustomerRepository cr = new CustomerRepository();
+            cr.CreateCustomer(customer);
+        }
+    }
+}
