7ce8fd7d826a0a1c9a2ca104093c4f5651e44752
Casper Søndergaard <casp4807@hotmail.com>
2017-01-17 08:54:46 +0100

    KeyGemerator File added

---
+++ PrettyHair.Core/Entities/EntityKeyGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrettyHair.Core.Entities
+{
+    class EntityKeyGenerator
+    {
+
+        private static volatile EntityKeyGenerator instance;
+
+        public static EntityKeyGenerator Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new EntityKeyGenerator();
+                }
+                return instance;
+            }
+        }
+        private int nextKey;
+
+        private EntityKeyGenerator()
+        { }
+
+        public virtual int NextKey
+        {
+            get
+            {
+                return ++nextKey;
+            }
+        }
+    }
+}
