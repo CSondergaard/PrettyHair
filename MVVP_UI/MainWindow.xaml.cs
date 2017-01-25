75b16807b876edba74a8dc1d60b83c1bc8c67cd1
Casper Søndergaard <casp4807@hotmail.com>
2017-01-25 11:05:28 +0100

    ez

--- MVVP_UI/MainWindow.xaml.cs
+++ MVVP_UI/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using PrettyHair.Core.Entities;
 using PrettyHair.Core.Repositories;
 using PrettyHair.Core.Helpers;
 using System.Data;
+using System.Windows.Forms;
 using System.Collections;
 
 namespace MVVP_UI
@@ -56,29 +57,29 @@ namespace MVVP_UI
             gridView.Columns.Add(new GridViewColumn
             {
                 Header = "ID",
-                DisplayMemberBinding = new Binding("Id")
+                DisplayMemberBinding = new System.Windows.Data.Binding("Id")
             });
             gridView.Columns.Add(new GridViewColumn
             {
                 Header = "Name",
-                DisplayMemberBinding = new Binding("Name")
+                DisplayMemberBinding = new System.Windows.Data.Binding("Name")
             });
             gridView.Columns.Add(new GridViewColumn
             {
                 Header = "Description",
-                DisplayMemberBinding = new Binding("Desc")
+                DisplayMemberBinding = new System.Windows.Data.Binding("Desc")
             });
             gridView.Columns.Add(new GridViewColumn
             {
                 Header = "Amount",
-                DisplayMemberBinding = new Binding("Amount")
+                DisplayMemberBinding = new System.Windows.Data.Binding("Amount")
             });
             gridView.Columns.Add(new GridViewColumn
             {
                 Header = "Price",
-                DisplayMemberBinding = new Binding("Price")
+                DisplayMemberBinding = new System.Windows.Data.Binding("Price")
             });
-          
+
             PrintStock();
 
         }
@@ -99,20 +100,90 @@ namespace MVVP_UI
         {
 
             MyItem row = (MyItem)listView.SelectedItems[0];
+
             if (row != null)
             {
-                int id;
-                id = Convert.ToInt32(row.Id);
-                IR.RemoveItemByID(id);
+                DialogResult res = System.Windows.Forms.MessageBox.Show("Are you sure to delete this item ??",
+                                     "Confirm Delete!!",
+                                     MessageBoxButtons.YesNo);
+                switch (res)
+                {
+                    case System.Windows.Forms.DialogResult.Yes:
+                        int id;
+                        id = Convert.ToInt32(row.Id);
+                        IR.RemoveItemByID(id);
 
-                PrintStock();
+                        PrintStock();
+                        break;
+                    case System.Windows.Forms.DialogResult.No:
+                        break;
+                }
+                   
+               
             }
             else
             {
                 PrintStock();
             }
 
-           
+
+
+
+        }
+
+        private void button1_click(object sender, RoutedEventArgs e)
+        {
+
+            double price;
+            string desc;
+            string name;
+            int amount;
+            if (!string.IsNullOrWhiteSpace(txtName.Text))
+            {
+                if (!string.IsNullOrWhiteSpace(txtDesc.Text))
+                {
+                    if (!string.IsNullOrWhiteSpace(txtPrice.Text))
+                    {
+                        if (!string.IsNullOrWhiteSpace(txtAmount.Text))
+                        {
+                            if (double.TryParse(txtPrice.Text, out price))
+                            {
+
+                                if (int.TryParse(txtAmount.Text, out amount))
+                                {
+                                    desc = txtDesc.Text;
+                                    name = txtName.Text;
+                                    Item item = new Item(name, desc, price, amount);
+                                    IR.CreateItem(item);
+
+                                    PrintStock();
+                                    txtName.Text = "";
+                                    txtDesc.Text = "";
+                                    txtPrice.Text = "";
+                                    txtAmount.Text = "";
+                                    System.Windows.Forms.MessageBox.Show(name + " is now created");
+                                    
+                                    
+                                }
+                                else
+                                {
+                                   System.Windows.Forms.MessageBox.Show("Amount is not a number");
+                                }
+                            }
+                            else
+                                System.Windows.Forms.MessageBox.Show("Price is not a number");
+                        }
+                        else
+                            System.Windows.Forms.MessageBox.Show("You need to enter amount");
+                    }
+                    else
+                        System.Windows.Forms.MessageBox.Show("You need to enter price.");
+                }
+                else
+                    System.Windows.Forms.MessageBox.Show("You need to enter a description");
+            }
+            else
+                System.Windows.Forms.MessageBox.Show("You need to enter a name");
 
 
         }
@@ -156,5 +227,6 @@ namespace MVVP_UI
         {
 
         }
+
     }
 }
